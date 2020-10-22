2feddbdda012aeac8da160edf0455426a88b0597
GoYamada <go@MacBookPro2020.local>
2020-10-22 13:33:00 +0900

    更新

--- sw.js
+++ sw.js
@@ -1,3 +1,19 @@
-self.addEventListener('fetch', function(e) {
-  // ここは空でもOK
-})
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('install', (event) => {
+  console.log('インストール');
+  event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
